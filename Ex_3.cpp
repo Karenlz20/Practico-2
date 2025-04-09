0c418b158672da604079d7f5b4005c914030bd1e
Karenlz20 <landivarzambranakaren@gmail.com>
2025-04-09 07:13:35 -0400

    Create Ex_3.cpp

---
+++ Ex_3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+ int width, height;
+ cout << "Enter rectangle width and height" << endl;
+ cin >> width >> height;
+ cout << "Rectangle area is " << width * height << endl;
+    return 0;
+}

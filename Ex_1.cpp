dffb8bc100df0ea91023a883c867b2be1229a907
Karenlz20 <landivarzambranakaren@gmail.com>
2025-04-09 07:08:58 -0400

    Create Ex_1.cpp
    Ejercicio 1 terminado

---
+++ Ex_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+   	double num1, num2;
+   	cout << "Ingrese el primer número:" << endl;
+   	cin >> num1;
+   	cout << "Ingrese el segundo número:" << endl;
+cin >> num2;
+  	int sum = num1 + num2;
+  	cout << "La parte entera de la suma de los números es " << sum << endl;
+  	return 0;
+}
+

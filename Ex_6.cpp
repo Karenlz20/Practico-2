704a120b3852d595e7462217736d0f3f9a2b7a4c
Karenlz20 <landivarzambranakaren@gmail.com>
2025-04-09 07:16:24 -0400

    Create Ex_6.cpp

---
+++ Ex_6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+ using namespace std; 
+int main () {
+string nombre, apellido;
+cout << "Ingrese su nombre y apellido" << endl;
+cin >> nombre >> apellido;
+cout << apellido + ", " + nombre;
+    return 0;
+ }
+

c1d2292e52f6d833f515685bce3d1c0deebe364a
Karenlz20 <landivarzambranakaren@gmail.com>
2025-04-09 07:17:09 -0400

    Create Ex_7.cpp

---
+++ Ex_7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string libro1, libro2, libro3;
+    getline(cin, libro1);
+    getline(cin, libro2);
+    getline(cin, libro3);
+    cout << libro3 << endl;
+    cout << libro2 << endl;
+    cout << libro1 << endl;
+    
+    return 0;
+}

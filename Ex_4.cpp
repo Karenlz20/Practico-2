8a64e6b15ffad446a190dd6db4a7827634b4ed69
Karenlz20 <landivarzambranakaren@gmail.com>
2025-04-09 07:14:34 -0400

    Create Ex_4.cpp

---
+++ Ex_4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+ int x, y;
+ int z = 1;
+cout << "Ingrese dos valores" << endl;
+ cin >> x >> y;
+ cout << "Your answer: " << x + y + z;
+    return 0;
+}
